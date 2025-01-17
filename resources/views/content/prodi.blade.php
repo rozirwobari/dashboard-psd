756839b8fb89f08baf740b42ef8af263af502129
rozirwobari <2rozirwobari@gmail.com>
2025-01-17 07:36:14 +0700

    Update Geo Grafik

--- resources/views/content/prodi.blade.php
+++ resources/views/content/prodi.blade.php
@@ -37,6 +37,20 @@
         user-select: none;
     }
 </style>
+<style>
+    #map {
+        height: 500px;
+        width: 100%;
+        border: 1px solid #ccc;
+    }
+
+    .legend {
+        line-height: 25px;
+        color: #333;
+    }
+</style>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+    integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
 @endsection
 
 @section('content')
@@ -78,7 +92,8 @@
                         </div>
                     </div>
                     <!-- Tambahkan div wrapper untuk mengontrol ukuran canvas -->
-                    <div class="d-flex justify-content-center" style="position: relative; margin: 0; height: 75%; margin: auto;">
+                    <div class="d-flex justify-content-center"
+                        style="position: relative; margin: 0; height: 75%; margin: auto;">
                         <canvas id="rzw-piechart"></canvas>
                     </div>
                 </div>
@@ -89,7 +104,8 @@
                 <div class="card-body shadow">
                     <div class="d-sm-flex d-block align-items-center justify-content-between mb-9">
                         <div class="mb-3 mb-sm-0">
-                            <h5 class="card-title fw-semibold" style="text-shadow: 0px 0px 30px rgba(0, 128, 0, 0.6);">Pertumbuhan Mahasiswa</h5>
+                            <h5 class="card-title fw-semibold" style="text-shadow: 0px 0px 30px rgba(0, 128, 0, 0.6);">
+                                Pertumbuhan Mahasiswa</h5>
                         </div>
                         <div class="d-flex flex-column">
                             <div class="d-flex gap-2 align-items-center">
@@ -123,7 +139,7 @@
                                 Pertumbuhan Mahasiswa</h5>
                         </div>
                     </div>
-                    <div class="container">
+                    {{-- <div class="container">
                         <div class="controls">
                             <div class="checkbox-group">
                                 <div class="checkbox-item">
@@ -148,8 +164,10 @@
                                 </div>
                             </div>
                         </div>
+                    </div> --}}
+                    <div class="container mt-4">
+                        <div id="map"></div>
                     </div>
-                    <div id="map"></div>
                 </div>
             </div>
         </div>
@@ -159,7 +177,10 @@
 
 
 @section('script')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-datalabels/2.0.0/chartjs-plugin-datalabels.min.js"></script>
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+    integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-datalabels/2.0.0/chartjs-plugin-datalabels.min.js">
+</script>
 <script>
     Chart.register(ChartDataLabels);
 
@@ -259,34 +280,6 @@
 
     updateChart();
 
-    // Event listener untuk select
-    // document.querySelector('.form-select').addEventListener('change', function (e) {
-    //     const selectedYear = e.target.value; // Tahun yang dipilih
-    //     const yearIndex = originalData.labels.indexOf(selectedYear); // Indeks tahun
-
-    //     if (selectedYear === "") {
-    //         // Reset ke data asli jika memilih "All"
-    //         myBarChart.data.labels = [...originalData.labels]; // Clone labels
-    //         myBarChart.data.datasets.forEach((dataset, index) => {
-    //             dataset.data = [...originalData.datasets[index].data]; // Clone data
-    //         });
-    //     } else if (yearIndex !== -1) {
-    //         // Jika tahun valid
-    //         myBarChart.data.labels = [selectedYear, selectedYear]; // Duplikasi label untuk membentuk garis
-    //         myBarChart.data.datasets.forEach((dataset, index) => {
-    //             const value = originalData.datasets[index].data[yearIndex];
-    //             dataset.data = [value, value]; // Duplikasi nilai data
-    //         });
-    //     }
-
-    //     // Update chart
-    //     myBarChart.update(); // Refresh grafik
-    // });
-
-
-
-
-
 
 
 
@@ -343,20 +336,17 @@
             ]
         };
 
-        // Get select elements
         const startYear = document.getElementById('startYear');
         const endYear = document.getElementById('endYear');
 
-        // Populate select options from originalData.labels
         originalData.labels.forEach(year => {
             startYear.add(new Option(year, year));
             endYear.add(new Option(year, year));
         });
 
-        // Inisialisasi chart
         const myBarChart = new Chart(ctx, {
             type: 'line',
-            data: JSON.parse(JSON.stringify(originalData)), // Deep clone originalData
+            data: JSON.parse(JSON.stringify(originalData)),
             options: {
                 scales: {
                     y: {
@@ -366,7 +356,6 @@
             }
         });
 
-        // Function to update chart based on year range
         function updateChart() {
             if (!startYear.value || !endYear.value) return;
             
@@ -378,7 +367,6 @@
                 return;
             }
 
-            // Get data for selected range
             const selectedLabels = originalData.labels.slice(startIndex, endIndex + 1);
             const selectedData = originalData.datasets.map(dataset => ({
                 ...dataset,
@@ -391,16 +379,13 @@
             myBarChart.update();
         }
 
-        // Event listeners for select inputs
         startYear.addEventListener('change', updateChart);
         endYear.addEventListener('change', updateChart);
 
-        // Reset button
         document.getElementById('resetBtn').addEventListener('click', function() {
             startYear.value = '';
             endYear.value = '';
             
-            // Reset chart ke data asli
             myBarChart.data.labels = [...originalData.labels];
             myBarChart.data.datasets = originalData.datasets.map(dataset => ({
                 ...dataset,
@@ -408,6 +393,131 @@
             }));
             myBarChart.update();
         });
+    });
+</script>
+
+<script>
+    var southWest = L.latLng(-11.0, 94.0);
+    var northEast = L.latLng(6.0, 141.0);
+    var bounds = L.latLngBounds(southWest, northEast);
+    var map = L.map('map', {
+        maxBounds: bounds,
+        maxBoundsViscosity: 1.0,
+        minZoom: 4 
+    });
+
+    var MapsLayer = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_background/{z}/{x}/{y}{r}.{ext}', {
+        minZoom: 0,
+        maxZoom: 20,
+        attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        ext: 'png'
+    });
+
+    MapsLayer.addTo(map);
+
+    var CartoDB_Positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 20
+    });
+
+    CartoDB_Positron.addTo(map);
+
+    var dataSiswa = {
+        "JAWA BARAT": 1000000,
+        "BANTEN": 200000,
+        "JAKARTA RAYA": 800000
+    };
+
+    var provinsi = {
+        "JAWA BARAT": [-6.8897, 107.6405],
+        "BANTEN": [-6.4058, 106.0640],
+        "DKI JAKARTA": [-6.2088, 106.8456]
+    };
+
+    function getColor(provinsiName) {
+        var mahasiswa = dataSiswa[provinsiName.toUpperCase()];
+        if (!mahasiswa) return "#fff";
+        return mahasiswa > 900000 ? '#d94801' :
+        mahasiswa > 700000 ? '#f16913' :
+        mahasiswa > 500000 ? '#fd8d3c' :
+        mahasiswa > 300000 ? '#fdae6b' :
+        mahasiswa > 100000 ? '#fdd0a2' : 
+                              '#fee6ce';
+    }
+
+    function CariProvinsi() {
+        let maxSiswa = 0;
+        let provinsiMax = null;
+        
+        Object.entries(dataSiswa).forEach(([provinsi, students]) => {
+            if (mahasiswa > maxSiswa) {
+                maxSiswa = mahasiswa;
+                provinsiMax = provinsi;
+            }
+        });
+        
+        return provinsiMax;
+    }
+
+    fetch('https://raw.githubusercontent.com/superpikar/indonesia-geojson/master/indonesia.geojson')
+        .then(response => response.json())
+        .then(data => {
+            const geoJsonLayer = L.geoJSON(data, {
+                style: function(data) {
+                    return {
+                        fillColor: getColor(data.properties.state),
+                        color: "transparent",
+                        weight: 1,
+                        opacity: 1,
+                        fillOpacity: 0.7
+                    };
+                },
+                onEachFeature: function(data, layer) {
+                    const provinsiName = data.properties.state.toUpperCase();
+                    const studentCount = dataSiswa[provinsiName];
+                    layer.bindPopup(`
+                        <strong>${data.properties.state}</strong><br>
+                        ${studentCount ? 
+                            `Jumlah Siswa: ${studentCount.toLocaleString()}` : 
+                            'Data tidak tersedia'}
+                    `);
+
+                    if (provinsi[provinsiName]) {
+                        layer.feature.properties.center = provinsi[provinsiName];
+                    }
+                }
+            }).addTo(map);
+
+            const ProvinsiMahasiswa = CariProvinsi();
+            if (ProvinsiMahasiswa && provinsi[ProvinsiMahasiswa]) {
+                const coordinates = provinsi[ProvinsiMahasiswa];
+                map.setView(coordinates, 7);
+            }
+        });
+
+    var legend = L.control({position: 'bottomright'});
+    legend.onAdd = function(map) {
+        var div = L.DomUtil.create('div', 'info legend');
+        var grades = [900000, 700000, 500000, 300000, 100000, 0];
+        var colors = ['#d94801', '#f16913', '#fd8d3c', '#fdae6b', '#fdd0a2', '#fee6ce'];
+        
+        div.innerHTML = '<div style="background: white; padding: 10px; border-radius: 5px; border: 1px solid #ccc;">' +
+                       '<strong>Jumlah Siswa</strong><br>';
+        
+        for (var i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+                '<i style="background: ' + colors[i] + '; display: inline-block; width: 20px; height: 20px; margin-right: 5px;"></i> ' +
+                (grades[i] ? '> ' + grades[i].toLocaleString() : '< 100,000') + '<br>';
+        }
+        
+        div.innerHTML += '</div>';
+        return div;
+    };
+    legend.addTo(map);
+
+    map.on('drag', function() {
+        map.panInsideBounds(bounds, { animate: false });
     });
 </script>
 @endsection
